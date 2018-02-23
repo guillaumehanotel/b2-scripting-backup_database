d8cbfbdc6316ddcdf455e1c35deefc50878ce26f
vagrant <vagrant@debian-85-amd64>
2018-02-23 04:41:36 -0500

    var date & folder + choix user

--- backup.py
+++ backup.py
@@ -1,26 +1,49 @@
 #!/usr/bin/python3.4
 from subprocess import call
 import os
+import datetime
+
+# db are stored in : /var/lib/mysql
 
 
-#call(["ls", "-l"])
+##### Variables
+current_date = datetime.date.today()
+backup_folder = "/home/vagrant/Documents"
 
 
-def save_BDD():
+##### Functions
+def save_db():
 	# Sauvegarde de la BDD :
-	os.system('mysqldump -u appli_web -p appli_web > dump_appli_web_01.sql')
+	os.system('mysqldump -u root -perty appli_web > /home/vagrant/Documents/dump_appli_web_01.sql')
 
-def save_BDD_zip():
-	# Sauvegarde de la BDD compressé :
-	os.system('mysqldump -u appli_web -p appli_web | gzip -9 > dump_appli_web_01.sql.gz')
-
-def restore_BDD():
-	# Restauration de la BDD :
-	os.system('mysql -u appli_web -p appli_web < dump_appli_web_01.sql')
-
-def restore_BDD_from_zip():
-	# Restauration de la BDD avec fichier compressé :
-	os.system('gunzip < dump_appli_web_01.sql.gz | mysql -u appli_web -p appli_web')
+def save_all_db():
+	# Sauvegarde toutes les BDD :
+	os.system('mysqldump -u root -perty --all-databases --events >' +str(backup_folder)+str(current_date)+'-dump_all_db_01.sql')
 
 
-restore_BDD()
+# def save_BDD_zip():
+# 	# Sauvegarde de la BDD compressé :
+# 	os.system('mysqldump -u appli_web -p appli_web | gzip -9 > dump_appli_web_01.sql.gz')
+
+def restore_all_db():
+	# Restauration des BDD :
+	os.system('mysql -u root -p appli_web < dump_appli_web_01.sql')
+
+
+# def restore_BDD_from_zip():
+# 	# Restauration de la BDD avec fichier compressé :
+# 	os.system('gunzip < dump_appli_web_01.sql.gz | mysql -u appli_web -p appli_web')
+
+
+
+##### Main
+choix_user = input("Bienvenue dans l'utilitaire de gestion de base de données.\n\n\tTapez 1 pour sauvegarder l'ensemble de vos base de données.\n\tTapez 2 pour restaurer vos bases de donnees.\n")
+
+if  choix_user == "1":
+	print("\nSauvegarde de l'ensemble des bases de donnees...")
+	save_all_db()
+else:
+	print("toto")
+
+
+
