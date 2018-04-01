15f7af74c7bd9ce500a716aa9eb064dc19f9bfe8
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-04-01 22:42:05 +0000

    website install

---
+++ script_install/website_install.sh
@@ -0,0 +1,56 @@
+if [ -f /var/www/html/index.html ]; then
+	# Suppression default apache page
+	sudo rm /var/www/html/index.html
+fi
+
+
+
+if [ ! -d /var/www/html/appli_web/ ]; then
+	# Import du site web
+	git clone https://github.com/guillaumehanotel/appli_web $HOME/appli_web/
+
+	# Déplacement du site dans le serveur
+	sudo mv $HOME/appli_web/ /var/www/html
+
+	# Mettre l'utilisateur dans le groupe www-data
+	sudo usermod -g www-data $USER
+
+	# Change user & group of website
+	sudo chown -R www-data:www-data /var/www/html/appli_web/
+fi
+
+
+
+if [ ! -f /etc/apache2/sites-available/001-appli_web.conf ]; then
+
+	# Création d'un nouveau virtual Host
+	sudo cp $HOME/postgreSQL/script_install/001-appli_web.conf /etc/apache2/sites-available/
+
+	# Activation du virtual host
+	sudo a2ensite 001-appli_web
+
+	# Rechargement d'apache
+	sudo service apache2 reload 
+
+	# Activation du module Apache rewrite qui permet l'utilisation des fichiers .htaccess
+	sudo a2enmod rewrite
+
+	# Redémarrage d'apache
+	sudo service apache2 restart 
+
+	# Désactivation du virtual host par défaut
+	sudo a2dissite 000-default
+
+	# Modification de la configuration général d'apache 
+	# Suppression de la directive Indexes pour le dossier /var/www (permet de lister les fichiers si il ny a pas dindex)
+
+	sudo sed -i -e 's/Options Indexes FollowSymLinks/Options FollowSymLinks/' /etc/apache2/apache2.conf
+
+	# Rechargement d'apache
+	sudo service apache2 reload 
+
+fi
+
+
+
+
