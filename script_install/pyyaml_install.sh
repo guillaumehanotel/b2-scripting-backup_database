91dd436c51e4fffc8dec9ff68babaddcbaec1e65
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-04-02 00:23:05 +0000

    corr pyyaml

--- script_install/pyyaml_install.sh
+++ script_install/pyyaml_install.sh
@@ -1,3 +1,4 @@
 tar zxvf ../PyYAML-3.12.tar.gz > /dev/null 2>&1
 sudo chown -R $USER /usr/local/lib/python3.4
-python3 PyYAML-3.12/setup.py install > /dev/null 2>&1
+cd $HOME/postgreSQL/script_install/PyYAML-3.12 
+python3 setup.py install > /dev/null 2>&1
